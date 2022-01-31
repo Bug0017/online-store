4ac24c689ce27f43d7aa0fe0a71aeb4d72e0fb71
Surendar <bug0017@outlook.com>
2022-01-31 18:34:23 +0530

    added typeorm

---
+++ apps/api/src/app/typeorm/typeorm.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class TypeormModule {}
